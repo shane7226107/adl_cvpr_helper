6240c5d17b4c49f5c0f20d4edf90910b4cec570b
Shane <shane7226107@gmail.com>
2013-02-19 18:31:41 +0800

    Batch Program OK

--- batch_ADL_annotation_to_haar.m
+++ batch_ADL_annotation_to_haar.m
@@ -1,5 +1,19 @@
-function batch_ADL_annotation_to_haar()
-    total_count = ADL_annotation_to_haar(1, 4, true,true)
-    total_count = ADL_annotation_to_haar(2, 4, true,true,total_count)
-    total_count = ADL_annotation_to_haar(3, 4, true,true,total_count)
+function batch_ADL_annotation_to_haar(video_index_array , obj_index , show , debug)
+    
+    fprintf('running Batch ADL_annotation_to_haar_info\n'); 
+    
+    % Accumulating counter
+    total_count = 0;
+    
+    for i=1:size(video_index_array,2)
+        
+        fprintf('\n\n\ngrabbing video: P_%02d.MP4    %d/%d in video array \n', video_index_array(1,i), i,size(video_index_array,2));
+        
+        if i == 1
+            total_count = ADL_annotation_to_haar(video_index_array(1,i), obj_index, show ,debug);
+        else
+            total_count = ADL_annotation_to_haar(video_index_array(1,i), obj_index, show ,debug,total_count);
+        end        
+        
+    end
 end
