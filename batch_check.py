25da9c3e58becdd9f6ca4aa89f8449ef9bfae82c
Shane Liu <shane7226107@gmail.com>
2013-03-08 10:50:48 +0800

    batch_check.py added

---
+++ batch_check.py
@@ -0,0 +1,41 @@
+import os
+
+#sub-routine used for appending log msgs
+def append_log(p,log):
+        while 1:
+                line = p.readline()
+                if not line: break
+                log.append(line)
+        return log
+
+print 'Batch checking...'
+
+#get all sub dir 
+all_subdirs = [name for name in os.listdir(".") if os.path.isdir(name)]
+
+print all_subdirs
+
+for subdir in all_subdirs:
+        print "\nProcessing folder:",subdir
+        
+        if subdir == ".git":
+                print "skipping", subdir
+                continue
+
+        #flag
+        find_cascade_file = False
+
+        os.chdir(subdir + "/classifier")
+
+        for files in os.listdir("."):
+                if files == "cascade.xml":
+                        print "find",files
+                        find_cascade_file = True
+                        break
+
+        os.chdir("../..")
+
+        if not find_cascade_file:
+                print subdir, "is not successful trianed!"
+
+print '\n\nBatch checking is done.'
