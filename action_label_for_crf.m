48d2879743a838805d7d57ca47388b7dc187315d
Shane Liu <shane7226107@gmail.com>
2013-03-28 17:18:53 +0800

    tmp

--- action_label_for_crf.m
+++ action_label_for_crf.m
@@ -7,8 +7,8 @@ function action_label_for_crf()
     action_counter = zeros(1,32);
     video_counter = 0;
     
-    %(obj_annotation , obj duration , video_index)
-    obj_table = ones(500,2,89)*-1;
+    %(obj_annotation , obj duration and video shown, obj_index)
+    obj_table = ones(500,3,89)*-1;
     obj_counter = zeros(1,89);
     
     %Load action annotation for each video
@@ -45,16 +45,20 @@ function action_label_for_crf()
     
     save('action_table.mat','action_table');
     
+    
+    
     %Load obj annotation for each video
     dir_name = '../ADL_annotations/object_annotation/translated_2';
     listing = dir(dir_name);
     
-    
+    video_counter = 0;
     
     for i=1:size(listing,1)
         if(strcmp(listing(i).name,'.') || strcmp(listing(i).name,'..') || strcmp(listing(i).name,'.DS_Store'))
             continue;
         end
+        
+        video_counter = video_counter + 1;
         
         fprintf('\n=============\n processing : %s\n',listing(i).name);
         obj_annotation = obj_annotation_read([dir_name '/' listing(i).name]);        
@@ -73,6 +77,7 @@ function action_label_for_crf()
             
             if obj_index == last_obj
                 last_frame = frame_index;
+                obj_table(obj_counter(1,obj_index),3,obj_index) = video_counter;
             else
                 obj_counter(1,obj_index) = obj_counter(1,obj_index) + 1;
                 if last_obj == -1
@@ -80,10 +85,12 @@ function action_label_for_crf()
                     obj_table(obj_counter(1,obj_index),1,obj_index) = frame_index;
                     %For those with only 1 line annotation
                     obj_table(obj_counter(1,obj_index),2,obj_index) = frame_index + 30;
+                    obj_table(obj_counter(1,obj_index),3,obj_index) = video_counter;
                     
                     last_obj = obj_index;
                 else
                     obj_table(obj_counter(1,last_obj),2,last_obj) = last_frame;
+                    obj_table(obj_counter(1,last_obj),3,last_obj) = video_counter;
                     
                     obj_table(obj_counter(1,obj_index),1,obj_index) = frame_index;
                     %For those with only 1 line annotation
@@ -102,9 +109,50 @@ function action_label_for_crf()
     %observations
     %(action_annotation , action time , video_index)    
     %(obj_annotation , obj duration , video_index)
-    % stageful action [9, 12, 13, 16]
+    stageful_action  = [9, 12, 13, 16];
     action_observation_table = ones(100,89,32)*-1;
+    action_observation_counter = zeros(1,32);
     
+    for action = 1:32
+        if ~isempty(find(stageful_action==action, 1))
+            fprintf('stageful action %d skipped\n',action);
+            continue;
+        end 
+        
+        fprintf('\n===============================\n proceesing : action %d\n',action);
+        
+        for i=1:action_counter(1,action)           
+            
+            m_start = action_table(i,1,action);
+            s_start = action_table(i,2,action);
+            m_end = action_table(i,3,action);
+            s_end = action_table(i,4,action);
+            video_index = action_table(i,5,action);
+            %Approximation here..
+            start_frame = round(m_start*60*29.97 + s_start*29.97);
+            end_frame = round(m_end*60*29.97 + s_end*29.97);
+            
+            fprintf('%d:%d -> %d:%d in video %d\n',m_start,s_start,m_end,s_end,video_index);
+            fprintf('frame:%d -> %d\n',start_frame,end_frame);
+            
+            action_observation_counter(1,action) = action_observation_counter(1,action) + 1;      
+            
+            for obj=1:89
+                for j=1:obj_counter(1,obj)
+                    if obj_table(j,3,obj) == video_index
+                        %fprintf('obj %d shown in video %d for action %d\n',obj,video_index,action);
+                        %fprintf('obj %d shown in video %d%d\n',obj,video_index);
+                        if abs(start_frame-obj_table(j,1,obj)) < 30 && abs(end_frame-obj_table(j,2,obj))
+                            fprintf('obj %d shown in video %d for action %d\n',obj,video_index,action);
+                        end
+                    end
+                end
+            end
+            
+        end
+    end
+    
+    save('action_observation_table.mat','action_observation_table');
     
 end
 
