d0a1b7e58f6355651d34631cecb266123806edaa
Shane Liu <shane7226107@gmail.com>
2013-04-04 22:19:02 +0800

    5-fold, multi_stage models added

--- fold_evaluation.m
+++ fold_evaluation.m
@@ -2,8 +2,8 @@ fold = 5;
 score = [];
 
 for i = 1:fold
-    ['cross_valid/result/fold_' int2str(i) '_result.txt']
-    score = [score crf_evaluation(['cross_valid/result/fold_' int2str(i) '_result.txt'])];
+    %score = [score crf_evaluation(['cross_valid/result/unigram/fold_' int2str(i) '_result.txt'])];
+    score = [score crf_evaluation(['cross_valid_complex/result/bigram/fold_' int2str(i) '_result.txt'])];
 end
 
 avg = mean(score)
