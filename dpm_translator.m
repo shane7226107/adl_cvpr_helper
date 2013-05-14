43b4d3b7e717efe4fe3f3ee56ab3737b629b456a
Shane Liu <shane7226107@gmail.com>
2013-05-14 15:42:48 +0800

    subfolder reader

--- dpm_translator.m
+++ dpm_translator.m
@@ -1,7 +1,25 @@
 function dpm_translator()
     
-    for video=1:20
-        fprintf(' video : %d\n',video);
-    end
+
+%     for video=1:1
+%         fprintf(' video : %d\n',video);
+%         load('../')
+%     end
     
+    %testset active
+    subfolders('../ADL_detected_objects/trainset/active/')
+    %testset passive    
+    subfolders('../ADL_detected_objects/trainset/passive/')
+    %train set active
+    subfolders('../ADL_detected_objects/testset/active/')
+    %train set passive
+    subfolders('../ADL_detected_objects/testset/passive/')
+end
+
+function subdir = subfolders(path)
+    d = dir(path);
+    isub = [d(:).isdir]; % returns logical vector indicates folders
+    nameFolds = {d(isub).name}'; % folders only
+    nameFolds(ismember(nameFolds,{'.','..'})) = []; %remove . and ..
+    subdir = nameFolds;
 end
