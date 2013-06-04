c690c80c2c8cb353416f0ca408a9f992feefbba9
Shane Liu <shane7226107@gmail.com>
2013-06-04 12:22:22 +0800

    obj and action annotation read ok

--- my_data/action_annotation_to_crf_training.m
+++ my_data/action_annotation_to_crf_training.m
@@ -1,3 +1,45 @@
 function action_annotation_to_crf_training()
+
     fprintf('action_annotation_to_crf_training\n');
+    
+    
+    for video=1:1
+        obj_annotation = obj_annotation_read(video);
+        action_annotation = action_annotation_read(video);
+    end
+    
+end
+
+
+
+function obj_annotation = obj_annotation_read(index)    
+    
+    filename = sprintf('annotations/objects/P%02d.mp4_label.txt',index);
+    
+    fprintf('reading: %s\n', filename);
+    
+    fid = fopen(filename);
+    %236 147 387 290 000001 0 1
+    obj_annotation = textscan(fid, '%d %d %d %d %d %d %d');
+    %Usage:
+    %obj_anno{col}(row)
+    %obj_anno{7}(1)
+    fclose(fid);
+    fprintf('finished reading objects annotation file\n');
+end
+
+function action_annotation = action_annotation_read(index)    
+    
+    filename = sprintf('annotations/action/P%02d.txt',index);
+    
+    fprintf('reading: %s\n', filename);
+    
+    fid = fopen(filename);
+    %001021 001351 4 drink_water
+    action_annotation = textscan(fid, '%d %d %d %s');
+    %Usage:
+    %obj_anno{col}(row)
+    %obj_anno{7}(1)
+    fclose(fid);
+    fprintf('finished reading action annotation file\n');
 end
