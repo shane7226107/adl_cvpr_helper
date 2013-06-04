cf01a5f71e9eb8aabc9b66ba5d4a83994e613e40
Shane Liu <shane7226107@gmail.com>
2013-06-05 01:32:54 +0800

    batch_FP_ADL_evaluation.m added

---
+++ my_data/batch_FP_ADL_evaluation.m
@@ -0,0 +1,32 @@
+function batch_FP_ADL_evaluation(thres_range)
+
+precision_list_no_pyramid = [];
+recall_list_no_pyramid = [];
+precision_list_pyramid = [];
+recall_list_pyramid = [];
+
+for thres=thres_range
+    
+   [recall,precision] = FP_ADL_evaluation(thres,0);
+   precision_list_no_pyramid = [precision_list_no_pyramid precision];
+   recall_list_no_pyramid = [recall_list_no_pyramid recall];
+   
+   [recall,precision] = FP_ADL_evaluation(thres,1);
+   precision_list_pyramid = [precision_list_pyramid precision];
+   recall_list_pyramid = [recall_list_pyramid recall];
+   
+end
+
+recall_list_no_pyramid
+
+plot(recall_list_no_pyramid,precision_list_no_pyramid,'-.s','Markersize',6,'MarkerFaceColor','k','LineWidth',3);
+        
+hold all
+
+plot(recall_list_pyramid ,precision_list_pyramid,'-rs','Markersize',6,'MarkerFaceColor','b','LineWidth',3);
+
+xlabel('Recall');
+ylabel('Precision');
+
+end
+
